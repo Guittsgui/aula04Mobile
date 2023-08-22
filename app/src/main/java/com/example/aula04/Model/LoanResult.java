16ac634d17d140e184d1621fbf956eea3ded678c
Guilherme Tavares <guilhermet.silva@viannasempre.com.br>
2023-08-22 14:35:22 -0300

    intent result criada, loan já passada, dados já comecando a serem preenchidos

--- app/src/main/java/com/example/aula04/Model/LoanResult.java
+++ app/src/main/java/com/example/aula04/Model/LoanResult.java
@@ -3,20 +3,31 @@ package com.example.aula04.Model;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.aula04.R;
 
 public class LoanResult extends AppCompatActivity {
     
-    
+    TextView loanValueField, loanFeeField, loanMonthsField, loanEntryField;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loan_result);
         
         binding();
+        Loan mainLoan = (Loan) getIntent().getSerializableExtra("loan");
+
+        loanValueField.setText("Você solicitou um empréstimo de: R$" + mainLoan.getLoanValue() );
+        loanFeeField.setText("Taxa de juros contratada de: " + mainLoan.getLoanFees() + "%");
+        loanMonthsField.setText("Pagamento será realizado em " + mainLoan.getQuantityOfMonths()+ " meses");
+        loanEntryField.setText("Entrada realizada no valor de: R$" + mainLoan.getEntryValue());
     }
 
     private void binding() {
+        loanValueField = findViewById(R.id.loanValueTV);
+        loanFeeField = findViewById(R.id.loanFeeTV);
+        loanMonthsField = findViewById(R.id.monthsQtTV);
+        loanEntryField = findViewById(R.id.entryValueTV);
     }
 }
